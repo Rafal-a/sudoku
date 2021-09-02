25c3bdb45b267e6eeea57e310c25f7c2aaabbef4
Rafal A <rafal.akeel99@gmail.com>
2021-09-03 01:21:30 +0300

    add functionality to the board

---
+++ app/src/main/java/com/thechance/sudoku/Solver.kt
@@ -0,0 +1,8 @@
+package com.thechance.sudoku
+
+class Solver(selectedColumn: Int) {
+    private val selectedRow:Int = -1
+
+    private val selectedColumn:Int = -1
+
+}
--- app/src/main/java/com/thechance/sudoku/SudokuBoard.kt
+++ app/src/main/java/com/thechance/sudoku/SudokuBoard.kt
@@ -6,6 +6,7 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 import kotlin.math.min
 
@@ -15,7 +16,8 @@ class SudokuBoard (context: Context, attributeSet: AttributeSet?) : View(context
     private  val squareSize = 3
     private  val size = 9
     private var cellSizePixel = 0f
-
+    private var selectedRow = 0
+    private var selectedColumn = 0
     private val thickLinePaint = Paint().apply {
         style =Paint.Style.STROKE
         strokeWidth= 4F
@@ -25,6 +27,16 @@ class SudokuBoard (context: Context, attributeSet: AttributeSet?) : View(context
         style =Paint.Style.STROKE
         strokeWidth= 2F
         color = Color.BLACK
+    }
+    private  var selectedCellPaint = Paint().apply {
+        style =Paint.Style.FILL_AND_STROKE
+        strokeWidth= 2F
+        color = Color.parseColor("#bbdefb")
+    }
+    private  var conflictedCellPaint = Paint().apply {
+        style =Paint.Style.FILL_AND_STROKE
+        strokeWidth= 2F
+        color = Color.parseColor("#e2ebf3")
     }
 
 
@@ -42,10 +54,39 @@ class SudokuBoard (context: Context, attributeSet: AttributeSet?) : View(context
         cellSizePixel = (width/size).toFloat()
         //draw the rect and the board
         canvas.drawRect(0f, 0f, width.toFloat(), height.toFloat(), thickLinePaint)
+        fillCells(canvas)
         drawLines(canvas)
     }
+    //draw in the cell
+    private fun fillCells(canvas: Canvas) {
+        //nothing was selected
+        if(selectedRow == -1  || selectedColumn == -1 )return
+        for( row in 0 .. size ){
+            for(column in 0 .. size){
+                if(row == selectedRow && column == selectedColumn){
+                    fillCell(canvas , row , column , selectedCellPaint)
+                } else if(row == selectedRow || column == selectedColumn)
+                    fillCell(canvas , row , column , conflictedCellPaint)
+                //cell in the same group
+                else if( row / squareSize == selectedRow/squareSize && column/squareSize ==selectedColumn/squareSize )
+                    fillCell(canvas , row , column , conflictedCellPaint)
 
-      private fun drawLines(canvas: Canvas) {
+            }
+        }
+
+    }
+
+    private fun fillCell(canvas: Canvas, row: Int, column: Int, paint: Paint) {
+        canvas.drawRect(
+            column * cellSizePixel,
+            row * cellSizePixel,
+            (column + 1) * cellSizePixel,
+            (row + 1) * cellSizePixel,
+            paint
+        )
+    }
+
+    private fun drawLines(canvas: Canvas) {
 
           for (i in 1 until size) {
              val paintToUse = when(i % squareSize){
@@ -70,6 +111,22 @@ class SudokuBoard (context: Context, attributeSet: AttributeSet?) : View(context
           }
 
       }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        return when(event!!.action){
+            MotionEvent.ACTION_DOWN-> {handleTouchEvent(event.x , event.y , )
+                true }
+            else -> false
+        }
+    }
+
+    private fun handleTouchEvent(x: Float, y: Float) {
+        selectedRow = (y/cellSizePixel).toInt()
+        selectedColumn = (x/cellSizePixel).toInt()
+        // will redraw the lines
+        invalidate()
+
+    }
 
 
 }
